a7f39cb042fd0048e7f3f4c617d8f47fbe6e466f
usiqwerty <benlogin1@protonmail.com>
2025-05-28 18:27:10 +0500

    fix: huge exception on exit

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import argparse
 import asyncio
+import os
+import signal
+import time
 
 from asyncio.exceptions import CancelledError
 
 from agents import Agent, Runner, set_default_openai_client, set_default_openai_api, set_tracing_disabled, RunConfig, \
-    RunResult
-from agents.mcp import MCPServerSse
+    RunResult, OpenAIChatCompletionsModel
+from agents.mcp import MCPServerSse, MCPServerStreamableHttp
 from agents.model_settings import ModelSettings
 from fastmcp import FastMCP
 from openai import AsyncOpenAI
@@ -14,11 +17,11 @@ from config import API_KEY, BASE_URL, MODEL_NAME
 from ui import logo
 from vibe_mcp import mcp_server
 
-model = AsyncOpenAI(
+client = AsyncOpenAI(
     api_key=API_KEY,
     base_url=BASE_URL
 )
-set_default_openai_client(model, use_for_tracing=False)
+set_default_openai_client(client, use_for_tracing=False)
 set_default_openai_api("chat_completions")
 set_tracing_disabled(disabled=True)
 
@@ -37,6 +40,7 @@ class VibeService:
             },
         )
 
+
     async def run_agent(self):
         async with self.server:
             agent = Agent(
@@ -46,7 +50,6 @@ class VibeService:
                 model_settings=ModelSettings(tool_choice="auto"),
             )
 
-            prev = None
             history = []
             while True:
                 message = input(f"{self.model_name}@{self.filename}> ")
@@ -55,29 +58,32 @@ class VibeService:
                     break
                 print(f"Running: {message}")
                 history.append({'type': 'message', 'role': 'user', 'content': message})
-                run_config = RunConfig()
-                run_config.model_settings = ModelSettings(max_tokens=400)
+                run_config = RunConfig(model=OpenAIChatCompletionsModel(model=MODEL_NAME, openai_client=client))
+
+                run_config.model_settings = ModelSettings(max_tokens=1000)
                 result = await Runner.run(starting_agent=agent,
                                           input=history,
-                                          run_config=run_config,
-                                          previous_response_id=prev)
-                prev = result.last_response_id
+                                          run_config=run_config)
                 result: RunResult
 
                 history = result.to_input_list()
                 print('LLM:', result.final_output)
 
     async def main(self):
-        self.server_task = asyncio.create_task(self.fast_mcp_server.run_sse_async())
-        self.agent_task = asyncio.create_task(self.run_agent())
+        server_task = asyncio.create_task(self.fast_mcp_server.run_http_async(transport='sse', log_level='warning'))
+        agent_task = asyncio.create_task(self.run_agent())
         try:
-            await self.agent_task
+            await agent_task
         finally:
-            self.server_task.cancel()
+            server_task.cancel()
             try:
-                await self.server_task
+                await server_task
             except CancelledError:
-                print("server cancelled")
+                print("Server task cancelled")
+
+        # Dirty trick, to deal with uvicorn's event loop
+        os.kill(os.getpid(), signal.SIGTERM)
+        print("Process was running after SIGTERM")
 
 
 if __name__ == "__main__":
