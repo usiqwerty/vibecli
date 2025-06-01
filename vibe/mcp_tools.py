0ddf54e66752adbab9dcef776f6f112666d4302c
usiqwerty <benlogin1@protonmail.com>
2025-06-01 19:18:38 +0500

    feat: check that tools work under current directory

--- vibe/mcp_tools.py
+++ vibe/mcp_tools.py
@@ -1,8 +1,11 @@
 import os
+from pathlib import Path
 import subprocess
 
 from fastmcp import FastMCP
+from fastmcp.exceptions import ToolError
 
+WORKING_DIRECTORY = Path(os.path.curdir).resolve()
 mcp_server = FastMCP(
     name="Vibe Server",
     instructions="""This server provides folowwing tools for programming:
@@ -18,6 +21,15 @@ mcp_server = FastMCP(
 )
 
 
+def check_path_under_cur_dir(path_string: str | None):
+    if path_string is None:
+        return
+
+    path = Path(path_string).resolve()
+    if WORKING_DIRECTORY not in path.parents:
+        raise ToolError("You are not allowed to work outside working directory")
+
+
 @mcp_server.tool()
 def run_tests() -> str:
     """run `pytest` command"""
@@ -31,6 +43,7 @@ def run_tests() -> str:
 @mcp_server.tool()
 def write_file(filename: str, content: str) -> int:
     """Write text to file"""
+    check_path_under_cur_dir(filename)
     print(f"Writing file {filename}...")
     consent = input('"y" to approve: ')
     if consent != "y":
@@ -38,16 +51,20 @@ def write_file(filename: str, content: str) -> int:
     with open(filename, 'w', encoding='utf-8') as f:
         return f.write(content)
 
+
 @mcp_server.tool()
 def read_file(filename: str) -> str:
     """Read text from file"""
+    check_path_under_cur_dir(filename)
     print(f"Reading file {filename}...")
     with open(filename, encoding='utf-8') as f:
         return f.read() or ""
 
+
 @mcp_server.tool()
 def list_dir(dir_name: str | None = None) -> list[str]:
     """List files and subdirectories"""
+    check_path_under_cur_dir(dir_name)
     print(f"Reading directory {dir_name}...")
     entries = os.listdir(dir_name)
     for i, ent in enumerate(entries):
